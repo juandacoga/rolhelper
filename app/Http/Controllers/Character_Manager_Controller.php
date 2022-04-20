8c4b928735e0ca14af05443677f0971664c47ae9
Juan David Correa <51924336+juandacoga@users.noreply.github.com>
2022-04-20 19:15:42 +0000

    Create character manager controller

---
+++ app/Http/Controllers/Character_Manager_Controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class Character_Manager_Controller extends Controller
+{
+  /**
+   * Create a new controller instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+      //
+  }
+  
+  //
+  
+}
